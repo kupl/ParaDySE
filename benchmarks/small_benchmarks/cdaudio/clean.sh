d5fdffdf5e920c748693b59a1c120e8cd4d0b650
sooyoung <soo-1989@hanmail.net>
2017-12-07 14:25:16 +0900

    modify clean script

--- benchmarks/small_benchmarks/cdaudio/clean.sh
+++ benchmarks/small_benchmarks/cdaudio/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f *.o *.exe *.cil.c *.i
-rm -f cfg* branches dominator dominator_tree funcount idcount stmtcount features szd_execution input coverage covered*
+rm -f cfg* branches dominator_tree funcount idcount stmtcount features szd_execution input coverage covered*
